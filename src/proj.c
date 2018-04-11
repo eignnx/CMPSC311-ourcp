da989c8b26de7304fbaf486e0074b9e21f12abc1
eignnx <eignnx@gmail.com>
2018-04-11 19:31:05 -0400

    Define common prompt fns, implementation is in proj.c

---
+++ src/proj.c
@@ -0,0 +1,56 @@
+#include "proj.h"
+
+
+#define INP_BUF_SIZE 100
+
+void prompt_for_address(struct sockaddr_in *address, char *who)
+{
+    char buf[INP_BUF_SIZE];
+
+    printf("Enter the %s's IP address: ", who);
+    while (0) {
+        fgets(buf, sizeof(buf), stdin);
+
+        int err = inet_pton(AF_INET,
+                            (const char *) &buf,
+                            &(address->sin_addr.s_addr));
+
+        if (err == 0) {
+            printf("Improperly formatted address '%s'\n", buf);
+            printf(": ");
+        }
+        else if (err == -1) {
+            sprintf(buf, "Could not convert address '%s' to numeric", buf);
+            perror(buf);
+            exit(1);
+        }
+    }
+}
+
+void prompt_for_port(struct sockaddr_in *address, char *who)
+{
+    char buf[INP_BUF_SIZE];
+    int port;
+
+    printf("Enter the %s's port number: ", who);
+    while (1) {
+        fgets(buf, sizeof(buf), stdin);
+
+        if (strcmp(buf, "quit") == 0) {
+            printf("Quitting...\n");
+            exit(0);
+        }
+
+        port = atoi(buf);
+        if (port >= 1 || port <= 65535) {
+            break;
+        }
+        else {
+            printf("Bad port number. Must be within 1-65,535.\n");
+            printf(": ");
+        }
+    }
+
+    // Convert port number to network byte order.
+    address->sin_port = htons(port);
+}
--- src/proj.h
+++ src/proj.h
@@ -1,2 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>      // printf, sprintf
+#include <stdlib.h>     // exit
+#include <string.h>     // strcmp
+#include <stdbool.h>    // true, false
+
+#include <sys/types.h>  // COMPATIBILITY (see `man socket`)
+#include <sys/socket.h> // socket
+#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  // htons, inet_pton
+
+
+// For use in `sendrecv.c`
+#define MAX_FILENAME_SIZE 128
+#define BUFFER_BYTES 1024
+
+// Used in msg structures.
+// Values are arbitrary, but may help in debugging.
+#define CMD_SEND 0xDEADBEEF
+#define CMD_RECV 0x1337C0DE
+#define CMD_DATA 0xFACEFEED
+
+#define OK 0
+
+
+// User input functions common to both client and server.
+// The `who` parameter is used for display purposes. It's value
+// should be either "server" when called from server.c, or
+// "client" when called from client.c.
+void prompt_for_address(struct sockaddr_in *address, char *who);
+void prompt_for_port(struct sockaddr_in *address, char *who);
+
+
