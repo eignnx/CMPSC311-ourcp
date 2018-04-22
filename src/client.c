e268f8dd679b9108a3489cb302ff10fed6c2391c
loltopus <37413412+loltopus@users.noreply.github.com>
2018-04-22 18:45:46 -0400

    version 2.0

--- src/client.c
+++ src/client.c
@@ -1,58 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
 #include "proj.h"
 
-//#define test (0xAAAA0000 | 4)
-//#define test2 (0xBBBB0000 | 4)
-
 int main(char argc, int *argv[]){
-	char ip[25];
-	char port[10];
 	char sfile[MAX_FILENAME_SIZE];
 	char mtype[10];
 	
-	printf("Enter IP address of destination => ");
-	scanf("%s", ip);
-	printf("Enter port of destination => ");
-	scanf("%s", port);
-	printf("Enter name of file to send => ");
-	scanf("%s", sfile);
+	struct sockaddr_in temp;
+	
+	prompt_for_address(temp,"client");
+	prompt_for_port(temp,"client");
+	
 	printf("Enter copy type => ");
 	scanf("%s", mtype);
+	printf("Enter name of file to send => ");
+	scanf("%s", sfile);
 	
 	
-	struct sockaddr_in temp;
-	temp.sin_family = AF_INET;
-	int sport = atoi(port);
-	temp.sin_port = htons(sport);
-	int s = inet_pton(AF_INET, ip, &(temp.sin_addr.s_addr));
-	int ltemp = sizeof(temp);
+	union any_msg p1;
+	p1->any.msg_type = mtype;
+	union any_msg p2;
 	
-	//prompt_for_address(,"client");
-	//prompt_for_port(,"client");
+	int sd = temp.sin_port;
+	int fd;
 	
-	FILE *fp = fopen(sfile,"r");
-	
-	if(strcmp(mtype,"CMD_SEND") == 0){
-		if(fp){
+	if(strcmp(mtype, "CMD_SEND") == 0){
+		if(locate_file(sfile,fd)){
 			printf("File exists\n");
-			
-			if((sendto(s, sfile, sizeof(sfile), 0, (struct sockaddr *)&temp, ltemp)) == -1){
-				perror("Send Error");
-			}else{
-				printf("Send file name: %s\n", sfile);
-				printf("Send file length %lu\n", sizeof(sfile));
-				printf("Connection accepted from localhost\n");
-			}
 		}else{
-			printf("File does not exists\n");
+			printf("File does not exist\n");
 			exit(1);
 		}
 		
+		send_msg(sd, p1);
+		recv_msg(sd, p2, CMD_RESP);
+		
+		if(p2->resp_msg == 0){
+			sd = temp.sin_port; 
+			fd = open(sfile, O_RDWR);
+			send(file(sd, fd);
+			close(fd);
+		}else{
+			printf("Send Error\n");
+		}
 	}else{
-	
+		p1->msg_type = "CMD_RECV";
+		send_msg(sd, p1);
+		recv_msg(sd, p2, CMD_RESP);
+		
+		if(p2->resp_msg == 0){
+			int temp = p2->resp_msg.file_size;
+			recv_file(sd, sfile, temp);
+		}else{
+			printf("Recieve Error\n");
+		}
 	}
-	
-	fclose(fp);
+	close(sd);
 }
