e19d9f4d4d27950854d1c6c85e0a1b1332d4dd9e
AishwaryaPatankar <patankaraishwarya@yahoo.com>
2018-04-25 19:08:20 -0500

    Update server.c

--- src/server.c
+++ src/server.c
@@ -52,12 +52,12 @@ int main(int argc, char *argv[]) {
         //if the copy type is from client to server: enters a loop that reads the network input for the file contents, and writes the bytes read to the output file;
         if(buffer.send.msg_type == CMD_SEND){
             send_msg(sd, message);
-            recv_file(sd, "receivedFile", buffer.send.file_size);
+            recv_file(sd, buffer.send.filename, buffer.send.file_size);
         }
         // if the copy type is from server to client: enters a loop that writes the file contents to the network output
         else if (buffer.send.msg_type == CMD_RECV){
             recv_msg(sd, &buffer, CMD_RECV);
-            recv_file(sd, "sentFile", buffer.send.file_size);
+            recv_file(sd, buffer.send.filename, buffer.send.file_size);
             int fd = open(clientfd, O_RDWR);
             if(locate_file(buffer.send.filename, clientfd)){
                 send_file(sd, fd);
